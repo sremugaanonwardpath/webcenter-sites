eb3f880bccaa37e0450438ff1639e570943d21d0
Sremugaan <sre93mugan@gmail.com>
2018-05-04 15:02:42 +0300

    pall changes

--- Test/src/test.java
+++ Test/src/test.java
@@ -4,7 +4,7 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 for(int i=0;i<5;i++){
-	System.out.println("changkjnkine code");
+	System.out.println("paalmugaan code");
 	
 }
 	}
